60d6f1083af692006bfeb1956922a088b3b44845
rposts <rishi.investigate@gmail.com>
2026-03-23 13:52:36 +0000

    Add distro processing

--- src/build.sh
+++ src/build.sh
@@ -3,6 +3,14 @@ set -e -o pipefail
 read -ra arr <<< "$@"
 version=${arr[1]}
 trap 0 1 2 ERR
+# Extract DISTRO details for tagging
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO="$ID-$VERSION_ID"
+    if [ "$VERSION_CODENAME" != "" ]; then
+        DISTRO="$ID-$VERSION_CODENAME"
+    fi
+fi
 # Ensure sudo is installed
 apt-get update && apt-get install sudo -y
 bash /tmp/linux-on-ibm-z-scripts/Spire/${version}/build_spire.sh -y
